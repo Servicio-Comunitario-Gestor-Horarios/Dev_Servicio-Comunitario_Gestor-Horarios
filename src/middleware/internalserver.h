733595231bb17938d6bc0c91a07b0f0b6b174b4a
magrmanuel25 <magrmanuel25@gmail.com>
2026-07-18 12:30:08 -0400

    Update internalserver.h

--- src/middleware/internalserver.h
+++ src/middleware/internalserver.h
@@ -8,16 +8,20 @@
 #include <QHash>
 #include <functional>
 
-// Constantes de respuesta
-const int RESP_OK = 0;
-const int RESP_INVALIDO = -1;
-const int RESP_TIEMPO_AGOTADO = -2;
+namespace Middleware {
 
+/**
+ * @class InternalServer
+ * @brief Gestiona las peticiones mediante un sistema de rutas QHash.
+ */
 class InternalServer : public QObject {
     Q_OBJECT
 public:
     explicit InternalServer(QObject *parent = nullptr);
     void processRequest(const QString &op, const QJsonObject &data);
+
+private slots:
+    void onNewConnection(); // Necesario para el punto C4
 
 private:
     QLocalServer *m_server;
@@ -26,19 +30,12 @@ private:
 
     void sendResponse(int status, const QJsonValue &data);
 
-    // Handlers (Todos los requeridos)
-    void handleTeacherList(); void handleTeacherGet(const QJsonObject &d);
-    void handleTeacherCreate(const QJsonObject &d); void handleTeacherUpdate(const QJsonObject &d);
-    void handleTeacherDelete(const QJsonObject &d);
-    void handleSubjectList(); void handleSubjectGet(const QJsonObject &d);
-    void handleSubjectCreate(const QJsonObject &d); void handleSubjectUpdate(const QJsonObject &d);
-    void handleSubjectDelete(const QJsonObject &d);
-    void handleClassroomList(); void handleClassroomGet(const QJsonObject &d);
-    void handleClassroomCreate(const QJsonObject &d); void handleClassroomUpdate(const QJsonObject &d);
-    void handleClassroomDelete(const QJsonObject &d);
-    void handlePlanList(); void handlePlanGet(const QJsonObject &d);
-    void handlePlanCreate(const QJsonObject &d); void handlePlanUpdate(const QJsonObject &d);
-    void handlePlanDelete(const QJsonObject &d);
-    void handleLoadAssign(const QJsonObject &d);
+    // Handlers (Se deben mantener para ser implementados en el .cpp)
+    void handleTeacherList();
+    void handleTeacherGet(const QJsonObject &d);
+    // ... (mantén todas las declaraciones de handlers que ya tienes)
 };
-#endif
+
+} // namespace Middleware
+
+#endif // INTERNALSERVER_H
