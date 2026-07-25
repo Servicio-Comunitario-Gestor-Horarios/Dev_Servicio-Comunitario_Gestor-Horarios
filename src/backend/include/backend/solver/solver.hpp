97f90c11b46747e8dcf2e3091e669c1a642c0cba
Luis <luisalexanderrojasguevara@gmail.com>
2026-07-25 11:59:04 -0400

    feat(backend): Header solver.hpp

---
+++ src/backend/include/backend/solver/solver.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "backend/data/horario_salida.hpp"
+#include "backend/solver/config/solver_config.hpp"
+
+class Solver {
+    public:
+        struct ResultadoSolver {
+            bool exito;
+            HorarioSalida resultado;
+            QStringList errores;
+            double tiempo_ms;
+        };
+
+        ResultadoSolver resolver(const SolverConfig& config);
+    
+    private:
+        void crearVariables(const SolverConfig& config);
+        void agregarRestricciones(const SolverConfig& config);
+        void extraerSolucion(const SolverConfig& config);
+};
